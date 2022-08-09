47371776269ebbfdfdf803d38d5a3f22c4fbaf2c
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-10 02:48:16 +0700

    Редактирование ивента

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import TripFiltersView from './view/trip-filters-view.js';
 import TripSortView from './view/trip-sort-view.js';
+import EventEditView from './view/event-edit.js';
 import {render} from './render.js';
 
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsSortElement = document.querySelector('.trip-events__trip-sort');
+const pageBodyContainerElement = document.querySelector('.page-body__page-main .page-body__container');
 
+/* Компоненты на своих местах */
 render(new TripFiltersView(), tripControlsFiltersElement);
 render(new TripSortView(), tripEventsSortElement);
+
+/* Компоненты временно отрендеренные в контейнере */
+render(new EventEditView(), pageBodyContainerElement);
