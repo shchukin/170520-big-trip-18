6d908f2c1ab60fa3d517d3d562e2ea158da7f63d
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-10 02:28:03 +0700

    Сортировка

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import TripFiltersView from './view/trip-filters-view.js';
+import TripSortView from './view/trip-sort-view.js';
 import {render} from './render.js';
 
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
+const tripEventsSortElement = document.querySelector('.trip-events__trip-sort');
 
 render(new TripFiltersView(), tripControlsFiltersElement);
+render(new TripSortView(), tripEventsSortElement);
