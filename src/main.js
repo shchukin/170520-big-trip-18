5794f4ff148f33dbcbe11bd29175b4ba254b20f9
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-10 02:53:26 +0700

    Ивент

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
 import TripFiltersView from './view/trip-filters-view.js';
 import TripSortView from './view/trip-sort-view.js';
+import EventView from './view/event.js';
 import EventEditView from './view/event-edit.js';
 import {render} from './render.js';
 
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
 const tripEventsSortElement = document.querySelector('.trip-events__trip-sort');
-const pageBodyContainerElement = document.querySelector('.page-body__page-main .page-body__container');
+const tripEventsElement = document.querySelector('.trip-events');
 
 /* Компоненты на своих местах */
 render(new TripFiltersView(), tripControlsFiltersElement);
 render(new TripSortView(), tripEventsSortElement);
 
-/* Компоненты временно отрендеренные в контейнере */
-render(new EventEditView(), pageBodyContainerElement);
+/* Компоненты временно отрендеренные в контейнер */
+render(new EventView(), tripEventsElement);
+render(new EventEditView(), tripEventsElement);
