07ecd3b4a7aa58593fc13bcfb9cf6f58dbdfa137
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-11 21:43:57 +0700

    Выносим в константу количество айтемов в списке

--- src/presenter/list-presenter.js
+++ src/presenter/list-presenter.js
@@ -2,13 +2,16 @@ import EventView from '../view/event.js';
 import EventEditView from '../view/event-edit.js';
 import {render} from '../render.js';
 
+const listItemsAmount = 3;
+
 export default class ListPresenter {
 
   init = (listPresenterContext) => {
 
+
     render(new EventEditView(), listPresenterContext);
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < listItemsAmount; i++) {
       render(new EventView(), listPresenterContext);
     }
 
