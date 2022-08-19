e57469c6a2d7ada41d41236fd420982035a47649
Anton Shchukin <a.a.shchukin@gmail.com>
2022-08-19 13:02:29 +0700

    Добавляем структуру данных задачи

---
+++ src/mock/event.js
@@ -0,0 +1,18 @@
+import {getRandomInteger} from '../utils.js';
+
+const generateType = () => {
+  const types = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+  const randomIndex = getRandomInteger(0, types.length - 1);
+  return types[randomIndex];
+};
+
+export const generatePoint = () => ({
+  basePrice: getRandomInteger(100, 100),
+  dateFrom: '2019-07-10T22:55:56.845Z',
+  dateTo: '2019-07-11T11:22:13.375Z',
+  destination: getRandomInteger(1, 100),
+  id: getRandomInteger(1, 100),
+  isFavorite: false,
+  offers: generateType,
+  type: generateType,
+});
